77b2aea957aba1426e247a96bc3df31eeeaa08fa
Nicolas Rouault <trouba@gmail.com>
2016-05-12 11:41:23 +0200

    add model validation

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,7 @@
 class Dose < ActiveRecord::Base
-  validates :description, presence: true
-  validates :cocktail, presence: true
-  validates :ingredient, presence: true
-  has_many :ingredients, through: :doses
   belongs_to :ingredient
   belongs_to :cocktail
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true, uniqueness: { scope: :cocktail}
 end
